991355e593f7715a5db068d3a088aed11b6a8ff2
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-25 15:00:03 +0800

    [Practice][Victor] Complete practice 05

--- src/main/java/practice05/Teacher.java
+++ src/main/java/practice05/Teacher.java
@@ -1,4 +1,30 @@
 package practice05;
 
-public class Teacher {
+public class Teacher extends Person {
+    private int klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = -1;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String result = super.introduce() + " I am a Teacher. I teach ";
+        if (getKlass() == -1) {
+            result += "No Class";
+        } else {
+            result += "Class " + getKlass();
+        }
+        result += ".";
+        return result;
+    }
 }
