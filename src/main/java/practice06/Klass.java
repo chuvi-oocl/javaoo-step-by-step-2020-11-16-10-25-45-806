b27c75153e3af0bf9b7d61704d72700d4efaaf16
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-25 15:18:45 +0800

    [Practice][Victor] Complete practice 06

--- src/main/java/practice06/Klass.java
+++ src/main/java/practice06/Klass.java
@@ -1,4 +1,23 @@
 package practice06;
 
 public class Klass {
+    private int klass;
+
+    public Klass(int klass) {
+        this.klass = klass;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public String getDisplayName() {
+        String result = "";
+        if (klass == -1) {
+            result += "No Class";
+        } else {
+            result += "Class " + klass;
+        }
+        return result;
+    }
 }
--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,32 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = new Klass(-1);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        return super.introduce() + " I am a Teacher. I teach " + klass.getDisplayName() + ".";
+    }
+
+    public String introduceWith(Student student) {
+        String result = super.introduce() + " I am a Teacher. I ";
+        if (student.getKlass() != klass) {
+            result += "don't ";
+        }
+        result += "teach " + student.getName() + ".";
+        return result;
+    }
 }
