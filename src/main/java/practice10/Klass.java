bd12bd9a7d2380672fbdbd46384e272c3fc3bae2
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-25 17:24:44 +0800

    [Practice][Victor] Complete practice 10

--- src/main/java/practice10/Klass.java
+++ src/main/java/practice10/Klass.java
@@ -1,4 +1,53 @@
 package practice10;
 
 public class Klass {
+    private int klass;
+    private Student leader;
+    private Teacher notifyingTeacher;
+
+
+    public Klass(int klass) {
+        this.klass = klass;
+        this.notifyingTeacher = null;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public void setNotifyingTeacher(Teacher notifyingTeacher) {
+        this.notifyingTeacher = notifyingTeacher;
+    }
+
+    public void assignLeader(Student student) {
+        if (student.getKlass() != this) {
+            System.out.print("It is not one of us.\n");
+        } else {
+            this.leader = student;
+            if (notifyingTeacher != null) {
+                notifyingTeacher.notifyMe(student.getName() + " become Leader of Class " + klass + ".");
+            }
+        }
+    }
+
+    public void appendMember(Student student) {
+        student.setKlass(this);
+        if (notifyingTeacher != null) {
+            notifyingTeacher.notifyMe(student.getName() + " has joined Class " + klass + ".");
+        }
+    }
+
+    public String getDisplayName() {
+        String result = "";
+        if (klass == -1) {
+            result += "No Class";
+        } else {
+            result += "Class " + klass;
+        }
+        return result;
+    }
 }
--- src/main/java/practice10/Teacher.java
+++ src/main/java/practice10/Teacher.java
@@ -1,4 +1,65 @@
 package practice10;
 
-public class Teacher {
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+
+public class Teacher extends Person {
+    private Klass klass;
+    private LinkedList<Klass> klasses;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+        this.klass = new Klass(-1);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+        this.klass.setNotifyingTeacher(this);
+        this.klasses = null;
+    }
+
+    public Teacher(int id, String name, int age, LinkedList<Klass> klasses) {
+        super(id, name, age);
+        this.klass = null;
+        this.klasses = klasses;
+        this.klasses.forEach(klass -> klass.setNotifyingTeacher(this));
+    }
+
+    public void notifyMe(String message) {
+        System.out.print("I am " + this.getName() + ". I know " + message + "\n");
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public LinkedList<Klass> getClasses() {
+        return klasses;
+    }
+
+    public Boolean isTeaching(Student student) {
+        return (student.getKlass() == this.klass || (klasses.stream().anyMatch(i -> i == student.getKlass())));
+    }
+
+    public String introduce() {
+        String result = super.introduce() + " I am a Teacher. I teach ";
+        if (klass != null) {
+            result += klass.getDisplayName() + ".";
+        } else if (klasses != null && klasses.size() > 0) {
+            result += "Class " + klasses.stream().map(Klass::getNumber).map(String::valueOf).collect(Collectors.joining(", ")) + ".";
+        } else {
+            result += "No Class.";
+        }
+        return result;
+    }
+
+    public String introduceWith(Student student) {
+        String result = super.introduce() + " I am a Teacher. I ";
+        if (!isTeaching(student)) {
+            result += "don't ";
+        }
+        result += "teach " + student.getName() + ".";
+        return result;
+    }
 }
