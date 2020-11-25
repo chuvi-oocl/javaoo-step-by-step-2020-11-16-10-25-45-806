3e6d3c005bd47fb3878846b8f0952f5c9184c919
VICTOR CHU (CLOUD-ISD-OOCLL/HKG) <victor.chu@oocl.com>
2020-11-25 15:58:10 +0800

    [Practice][Victor] Complete practice 08

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,42 @@
 package practice08;
 
 public class Klass {
+    private int klass;
+    private Student leader;
+
+    public Klass(int klass) {
+        this.klass = klass;
+        this.leader = null;
+    }
+
+    public int getNumber() {
+        return klass;
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+
+    public void assignLeader(Student student){
+        if(student.getKlass() != this){
+            System.out.print("It is not one of us.\n");
+        }
+        else {
+            this.leader = student;
+        }
+    }
+
+    public void appendMember(Student student){
+        student.setKlass(this);
+    }
+    public String getDisplayName() {
+        String result = "";
+        if (klass == -1) {
+            result += "No Class";
+        } else {
+            result += "Class " + klass;
+        }
+        return result;
+    }
+
 }
--- src/main/java/practice08/Student.java
+++ src/main/java/practice08/Student.java
@@ -1,4 +1,29 @@
 package practice08;
 
-public class Student {
+public class Student extends Person {
+    private Klass klass;
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public String introduce() {
+        String result = super.introduce() + " I am a Student. I am ";
+        if(klass.getLeader() == this){
+            result += "Leader of ";
+        }
+        else{
+            result += "at ";
+        }
+        result += klass.getDisplayName()+'.';
+        return result;
+    }
 }
